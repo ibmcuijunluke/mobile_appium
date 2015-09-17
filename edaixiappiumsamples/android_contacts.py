b0ed979cf6042b433e35a252f94a99af1c7db7b8
ibmcuijunluke <ibmcuijun2015@126.com>
2015-09-17 11:54:35 +0800

    submit some appium testcodes

--- edaixiappiumsamples/android_contacts.py
+++ edaixiappiumsamples/android_contacts.py
@@ -10,6 +10,8 @@ import os
 import unittest
 from appium import webdriver
 from time import sleep
+import nose
+import py.test
 
 # Returns abs path relative to this file and not cwd
 PATH = lambda p: os.path.abspath(
@@ -54,8 +56,49 @@ class ContactsAndroidTests(unittest.TestCase):
 
     self.driver.keyevent(3)
 
+class ContactsListAndroidTests(unittest.TestCase):
+  def setUp(self):
+    desired_caps = {}
+    desired_caps['platformName'] = 'Android'
+    desired_caps['platformVersion'] = '4.4'
+    desired_caps['deviceName'] = '192.168.56.111:5555'
+    desired_caps['app'] = PATH(
+      '../../../sample-code/apps/ContactManager/ContactManager.apk'
+    )
+    desired_caps['appPackage'] = 'com.example.android.contactmanager'
+    desired_caps['appActivity'] = '.ContactManager'
+
+    self.driver = webdriver.Remote('http://0.0.0.0:4723/wd/hub', desired_caps)
+
+  def tearDown(self):
+    self.driver.quit()
+
+  def test_add_contactslist(self):
+    el = self.driver.find_element_by_name("Add Contact")
+    el.click()
+
+    textfields = self.driver.find_elements_by_class_name("android.widget.EditText")
+    textfields[0].send_keys("Appium User")
+    textfields[2].send_keys("someone@appium.io")
+
+    self.assertEqual('Appium User', textfields[0].text)
+    self.assertEqual('someone@appium.io', textfields[2].text)
+
+    self.driver.find_element_by_name("Save").click()
+
+    # for some reason "save" breaks things
+    alert = self.driver.switch_to_alert()
+
+    # no way to handle alerts in Android
+    self.driver.find_element_by_android_uiautomator('new UiSelector().clickable(true)').click()
+
+    self.driver.keyevent(3)
+    
+    
+
 
 
 if __name__ == '__main__':
   suite = unittest.TestLoader().loadTestsFromTestCase(ContactsAndroidTests)
+  suite = unittest.TestLoader().loadTestsFromTestCase(ContactsListAndroidTests)
   unittest.TextTestRunner(verbosity=2).run(suite)
